2d839160884684fb7b4abd74f630eeca986a6d62
Nate Costello <natecostello@gmail.com>
2021-04-18 16:20:04 +0100

    fixed py_modules in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    py_modules=['uni_t.py'],
+    py_modules=['uni_t'],
     #package_dir={"": "src"},
     #packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
