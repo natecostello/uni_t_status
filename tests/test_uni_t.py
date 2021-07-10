cf4da70ef2fcf3136a50d5314ce7739b05dee808
Nate Costello <natecostello@gmail.com>
2021-07-10 17:17:29 +0000

    updated test file

--- tests/test_uni_t.py
+++ tests/test_uni_t.py
@@ -8,13 +8,19 @@ import timeit
 print("prior to runing the test, ensure all usb devices \
     are unplugged, then only plug in the DMM usb connector.")
 
-dmm = uni_t.DMMMonitor('volts', '/dev/ttyUSB0')
+dmm = uni_t.DMMMonitor('/dev/ttyUSB0')
+dmm.parametername = 'test_param_name'
 
 dmm.start()
 
 while not dmm.isValid:
     time.sleep(0.1)
-    
-while True:
-    print(str(dmm.value) + ' ' + dmm.units)
+
+print("Take 10 samples, one per second")
+count = 0
+while count < 10:
+    param = dmm.parameters[0]
+    measurement = dmm.allmeasurements[param]
+    print(param + ' : ' + measurement)
     time.sleep(1)
+    
